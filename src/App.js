929d9cdbb2eaf4c9ce7841076f34d1a9fb3fba94
Rasif <rasiftagizade11@gmail.com>
2022-09-12 21:39:00 +0400

    Bug fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from "./components/Header.jsx"
-import HeroSection from "components/HeroSection.jsx";
-import Categories from "components/Categories.jsx";
-import Compaigns from "components/Compaigns.jsx";
-import Favorites from "components/Favorites.jsx"; 
-import MobileApp from "components/MobileApp.jsx";
-import Cards from "components/Cards.jsx";
+import HeroSection from "./components/HeroSection.jsx";
+import Categories from "./components/Categories.jsx";
+import Compaigns from "./components/Compaigns.jsx";
+import Favorites from "./components/Favorites.jsx"; 
+import MobileApp from "./components/MobileApp.jsx";
+import Cards from "./components/Cards.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
--- src/components/MobileApp.jsx
+++ src/components/MobileApp.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 const MobileApp = () => {
   return (
     <div>MobileApp</div>
